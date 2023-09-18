d6717760f1b04ac9737e0ae171fa1f1a03ad0723
Lauren <lauren.a.heavey@gmail.com>
2023-09-18 13:50:08 -0500

    Add API route; still playing around with requests and responses.

---
+++ app/api/route.js
@@ -0,0 +1,5 @@
+// export async function GET(Request) {}
+// export async function HEAD(Request) {}
+// export async function POST(Request) {}
+// export async function PUT(Request) {}
+// export async function DELETE(Request) {}
